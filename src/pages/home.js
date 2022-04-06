5165a165307d7e6c25409257bd0fea7607d1997d
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2022-04-06 21:07:16 +0900

    fix: main title translate fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -30,39 +30,82 @@ export const Home = () => {
       </Helmet>
       <MainHeader />
       <HomeContainer>
-        <MainTemplate positionYA={offsetY}>
-          <MainTitle positionYA={offsetY}>THANKS FOR COMING!</MainTitle>
-        </MainTemplate>
+        <MainContainer positionYA={offsetY}>
+          <MainGreeting>
+            <MainGreetingText positionYA={offsetY}>안녕하세요, 저는 류정상입니다.</MainGreetingText>
+          </MainGreeting>
+          <MainGreeting>
+            <MainGreetingText positionYA={offsetY}>저는 프론트엔드 개발자입니다.</MainGreetingText>
+          </MainGreeting>
+          <MainTitle positionYA={offsetY}>
+            {/* 이동할 컨테이너 */}
+            <MainTitleText>THANKS FOR COMING!</MainTitleText>
+          </MainTitle>
+        </MainContainer>
+        <AboutContainer>abouts</AboutContainer>
+        <div>projects</div>
+        <div>contact</div>
       </HomeContainer>
     </>
   );
 };
 
 const HomeContainer = styled.div`
-  padding-top: 40px;
   background: linear-gradient(0deg, skyblue, salmon);
   width: 100%;
   height: 500vh;
   font-family: "Spoqa Han Sans Neo", "sans-serif";
+  overflow: hidden;
 `;
 
-const MainTemplate = styled.div`
+const MainContainer = styled.div`
   position: ${props => (props.positionYA > 800 ? `absolute` : `fixed`)};
   display: flex;
-  justify-content: center;
+  //justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: purple;
   transform: ${props => (props.positionYA > 800 ? `translateY(800px)` : `translateY(0px)`)};
+  overflow: hidden;
 `;
 
 const MainTitle = styled.div`
+  width: 100%;
+  transition: all 1s ease-in-out;
+  transform: ${props => (props.positionYA > 10 ? `translateX(52%)` : `translateX(0%)`)};
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainTitleText = styled.div`
   width: 450px;
   font-size: 48pt;
-  transition: all 1s ease-in-out;
-  transform: ${props => (props.positionYA > 10 ? `translateX(120%)` : `translateX(0%)`)};
   white-space: normal;
   font-weight: 700;
   line-height: 1.1em;
+`;
+
+const MainGreeting = styled.div`
+  width: 300px;
+  height: 500px;
+  overflow: hidden;
+`;
+
+const MainGreetingText = styled.div`
+  transition: all 1s ease-in-out;
+  transition-delay: 0.1s;
+  transform: ${props => (props.positionYA > 10 ? `translateY(0%)` : `translateY(-10px)`)};
+`;
+
+const AboutContainer = styled.div`
+  margin-top: 100vh;
+  position: absolute;
+  top: 100vh;
 `;
